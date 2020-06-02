7433e4e94d0cc3f2e73ba178066fce193e4bedd3
Dim <62505585+Dim9999@users.noreply.github.com>
2020-06-02 14:57:26 +0300

    Add files via upload

---
+++ src/Dim9999/KillRewards/Main.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dim9999\KillRewards;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+    public function onEnable(): void
+    {
+        $this->getLogger()->notice("Enabled");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+    }
+
+    private function rewardItems(Player $player): void
+    {
+        $items = $this->getConfig()->get("items");
+        if (!$this->getConfig()->get("random-items")) {
+            foreach ($this->getConfig()->get("items") as $item) {
+                $item = $items[$item];
+                $data = explode(":", $item);
+                $item = Item::get((int)$data[0], (int)$data[1], (int)$data[2]);
+                if (isset($data[3])) {
+                    $item->setCustomName((string)$data[3]);
+                }
+                $player->getInventory()->addItem(Item::get((int)$data[0], (int)$data[1], (int)$data[2]));
+            }
+        } else {
+            $item = array_rand($this->getConfig()->get("items"), 1);
+            $item = $items[$item];
+            $data = explode(":", $item);
+            $item = Item::get((int)$data[0], (int)$data[1], (int)$data[2]);
+            if (isset($data[3])) {
+                $item->setCustomName((string)$data[3]);
+            }
+            $player->getInventory()->addItem(Item::get((int)$data[0], (int)$data[1], (int)$data[2]));
+        }
+    }
+
+    private function rewardMoney(Player $player): void
+    {
+        $api = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        if (!$api) {
+            return;
+        }
+        if ($this->getConfig()->get("economyapi-support")) {
+            if (!$this->getConfig()->get("enable-random")) {
+                $api->addMoney($player, $this->getConfig()->get("money"));
+            } else {
+                $api->addMoney($player, mt_rand((int)$this->getConfig()->get("min-money"), (int)$this->getConfig()->get("max-money")));
+            }
+        }
+    }
+
+    /**
+     * @param Player $player
+     */
+    private function rewardCommands(Player $player): void
+    {
+        $commands = $this->getConfig()->get("commands");
+        if (!$this->getConfig()->get("random-command")) {
+            foreach ($this->getConfig()->get("commands") as $command) {
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", '"' . $player->getName() . '"', $commands[$command]));
+            }
+        } else {
+            $command = array_rand($this->getConfig()->get("commands"), 1);
+            $this->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", '"' . $player->getName() . '"', $commands[$command]));
+        }
+    }
+
+    /**
+     * @param EntityDamageEvent $event
+     */
+    public function onDamage(EntityDamageEvent $event): void
+    {
+        if ($event->isCancelled()) {
+            return;
+        }
+        $player = $event->getEntity();
+        if ($player instanceof Player and $event instanceof EntityDamageByEntityEvent and $event->getFinalDamage() >= $player->getHealth()) {
+            $killer = $event->getDamager();
+            if ($killer instanceof Player) {
+                if (!$this->getConfig()->get("enable-luck") or ($this->getConfig()->get("enable-luck") and mt_rand((int)$this->getConfig()->get("min"), (int)$this->getConfig()->get("max")) === (int)$this->getConfig()->get("value"))) {
+                    $this->rewardCommands($killer);
+                    $this->rewardItems($killer);
+                    $this->rewardMoney($killer);
+                }
+                if ($this->getConfig()->get("enable-messages")) {
+                    $killer->sendMessage(str_replace(["{killer}", "{player}"], [$killer->getName(), $player->getName()], $this->getConfig()->get("killer-message")));
+                    $player->sendMessage(str_replace(["{killer}", "{player}"], [$killer->getName(), $player->getName()], $this->getConfig()->get("player-message")));
+                }
+            }
+        }
+    }
+}
